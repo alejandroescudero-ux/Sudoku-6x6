7cc4db18ad2aedd20b8eb1d0ce12045861d8a1c5
LGZU5 <jisusdeivid@gmail.com>
2025-10-06 22:22:48 -0500

    creacion de modelo MVC, organizacion de carpetas y creacion de vista 'welcome view' inicial

--- src/main/java/project/sudoku/Main.java
+++ src/main/java/project/sudoku/Main.java
@@ -5,17 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import project.sudoku.views.WelcomeView;
+
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sudokuMain.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        Image icon = new Image(Main.class.getResourceAsStream("sudokuLogo.png"));
-        stage.getIcons().add(icon);
-        stage.setTitle("Sudoku 6x6");
-        stage.setScene(scene);
-        stage.show();
+        WelcomeView welcomeView =  WelcomeView.getInstance();
+        welcomeView.show();
     }
 }
---
+++ src/main/java/project/sudoku/views/WelcomeView.java
@@ -0,0 +1,39 @@
+package project.sudoku.views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomeView extends Stage {
+    public WelcomeView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/project/sudoku/sudokuMain.fxml")
+        );
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        this.setScene(scene);
+        this.setTitle("TypeBeat");
+        this.getIcons().add(new Image(
+                getClass().getResourceAsStream("/project/sudoku/images/sudokuLogo.png")
+        ));
+    }
+
+    public static WelcomeView getInstance() throws IOException {
+        if (WelcomeView.WelcomeViewHolder.INSTANCE == null) {
+            WelcomeView.WelcomeViewHolder.INSTANCE = new WelcomeView();
+        }
+        return WelcomeView.WelcomeViewHolder.INSTANCE;
+    }
+
+    /**
+     * Clase estática interna que implementa el patrón Holder
+     * para almacenar la instancia única de @link WelcomeView
+     */
+    private static class WelcomeViewHolder {
+        private static WelcomeView INSTANCE = null;
+    }
+}
